f1631593691c39fbfcf76f0419053772b8ffe364
Onos Ejoor <onosejoor14@gmail.com>
2024-06-18 09:47:51 +0100

    Add vercel app

---
+++ index.js
@@ -0,0 +1,65 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+// For short URL //
+let basrUrl = "https://cleanuri.com/api/v1/shorten";
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+let url = []
+// to render home page
+app.get("/", (req, res) => {
+  res.render("index.ejs", {
+    url: url
+  });
+  console.log(url);
+});
+
+// to create short url
+app.post("/short", async (req, res) => {
+
+  try {
+
+  let userUrl = req.body.longUrl;
+console.log(userUrl);
+
+  function truncateString(str, maxLength) {
+    if (str.length > maxLength) {
+        return str.slice(0, maxLength - 3) + '...';
+    }
+    return str;
+}
+
+let cater = truncateString(userUrl, 30);
+  let headersList = {
+    Accept: "*/*",
+    "Content-Type": "application/x-www-form-urlencoded",
+  };
+
+  const response = await axios.post(basrUrl, `url=${userUrl
+  }`, headersList);
+  console.log(response, response.data);
+
+  let result = response.data;
+
+  url.push({id: url.length+1, long: cater, short: result.result_url});
+  res.redirect("/");    
+
+  } catch (error) {
+    res.render("index.ejs", {
+      url: url,
+    });
+  }
+
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port} ✌`);
+});
+
+export default app;
---
+++ public/url.js
@@ -0,0 +1,54 @@
+let shortUrl = document.getElementsByClassName("sh").length;
+let copy = document.getElementsByClassName("copy");
+
+// For Nav Bar //
+
+function navBar() {
+  const nav = $(".nav-contents");
+  nav.toggleClass("toogle");
+}
+
+$(".svg-icon").click(() => {
+  navBar();
+});
+
+function change() {
+  let input = document.querySelector(".input");
+  let error = document.querySelector(".error");
+  if (input.value.length === 0) {
+    error.style.visibility = "visible";
+    input.classList.add("input2");
+  }
+}
+
+// for clipboard copy
+
+for (let i = 0; i < shortUrl; i++) {
+  let copied = document.getElementsByClassName("copy")[i];
+
+  copied.addEventListener("click",function () {
+
+    for (let j = 0; j < copy.length; j++) {
+    copy[j].classList.remove("copied");
+    $(this).addClass("copied");
+    $(".copy").text("Copy");
+    $(this).text("Copied!");
+    console.log(this);
+    }
+
+    // get the container
+    const element = document.querySelectorAll(".short")[i];
+
+    // Create a fake `textarea` and set the contents to the text
+    // you want to copy
+    const storage = document.createElement("textarea");
+    storage.value = element.innerHTML;
+    element.appendChild(storage);
+
+    // Copy the text in the fake `textarea` and remove the `textarea`
+    storage.select();
+    storage.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(storage.value);
+    element.removeChild(storage);
+  });
+}
