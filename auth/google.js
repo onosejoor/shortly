5290f2e964d7fe4922c735647f269e07a8db96c1
Onos <onosejoor14@gmail.com>
2024-09-05 16:01:17 +0100

    update code

--- auth/google.js
+++ auth/google.js
@@ -2,7 +2,25 @@ import passport from "./passport.js";
 import express from "express";
 const app = express();
 
-export const google = app.get("/oauth/google", passport.authenticate("google", {
-  successRedirect: "/",
-  failureRedirect: "/login"
-}));
+export const google = app.get("/oauth/google", (req, res, next) => {
+  passport.authenticate("google", (user, info, err) => {
+    if (err) {
+      res.render("index.ejs", {
+        error: err.message,
+      });
+    }
+    if (!user) {
+      res.render("index.ejs", {
+        err: info.message,
+      });
+    }
+    req.login(user, (err) => {
+      if (err) {
+        return next(err, false);
+      } else {
+        next(null, user);
+        res.redirect("/");
+      }
+    });
+  });
+});
