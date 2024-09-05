c65aea5484907758d9690ec1b203a1e97349562a
Onos <onosejoor14@gmail.com>
2024-09-05 17:08:26 +0100

    update google oauth

--- auth/google.js
+++ auth/google.js
@@ -1,6 +1,6 @@
 import passport from "./passport.js";
 import express from "express";
-import ejs from "ejs"
+import ejs from "ejs";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -10,32 +10,29 @@ app.set("view engine", "ejs");
 app.engine("ejs", ejs.__express); // Add this line to set the templating engine
 app.set("views", __dirname + "/views");
 
-export const google = app.get("/oauth/google",async (req, res, next) => {
+export const google = app.get("/oauth/google", async (req, res, next) => {
   try {
-  passport.authenticate("google", (user, info, err) => {
-    if (err) {
-      res.render("index.ejs", {
-        error: err.message,
-      });
-    }
-    if (!user) {
-      res.render("index.ejs", {
-        err: info.message,
-      });
-    }
-    req.login(user, (err) => {
+    passport.authenticate("google", (err, user, info) => {
       if (err) {
-        return next(err, false);
-      } else {
-        next(null, user);
-        res.redirect("/");
+        return next(err);
       }
-    });
-  })(req, res, next);    
+      if (!user) {
+        res.render("index.ejs", {
+          err: info.message,
+        });
+      }
+      req.login(user, (err) => {
+        if (err) {
+          return next(null, false);
+        } else {
+          next(null, user);
+          res.redirect("/");
+        }
+      });
+    })(req, res, next);
   } catch (error) {
     res.render("index.ejs", {
-      error: error
-    })
+      error: error,
+    });
   }
-
 });
